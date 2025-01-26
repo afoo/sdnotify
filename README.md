34a8d85e8d416141432cbcf6743485af88f23feb
Jan Hülsbergen <jan@afoo.de>
2025-01-26 13:05:22 +0100

    prepare beta7

---
+++ README.md
@@ -0,0 +1,15 @@
+# SDNotify
+
+A simple library for Java 8+ implementing the [sd_notify](https://www.freedesktop.org/software/systemd/man/sd_notify.html) protocol from systemd.
+
+```xml
+<dependency>
+    <groupId>de.afoo</groupId>
+    <artifactId>sdnotify</artifactId>
+    <version>1.0-beta7</version>
+</dependency>
+```
+
+```groovy
+implementation 'de.afoo:sdnotify:1.0-beta7'
+```
--- src/main/java/de/afoo/sdnotify/package-info.java
+++ src/main/java/de/afoo/sdnotify/package-info.java
@@ -5,6 +5,8 @@
  * <p>This does not use the systemd provided C library or commandline tool but instead directly
  * talks to the UNIX socket systemd provides.
  *
+ * <p>Git repository: <a href="https://github.com/afoo/sdnotify">https://github.com/afoo/sdnotify</a>
+ *
  * <p>In most cases you should use {@link de.afoo.sdnotify.SDNotify#create()} to get an instance.
  */
 package de.afoo.sdnotify;
